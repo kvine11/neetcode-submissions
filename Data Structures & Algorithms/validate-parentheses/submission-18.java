b013d551de329e59eb89ca4588b79d9e93f796a2
Kevin Liu <kvine2714@gmail.com>
2026-08-03 12:57:17 -0500

    Add: validate-parentheses - submission-18

---
+++ Data Structures & Algorithms/validate-parentheses/submission-18.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> open = new Stack<>();
+        HashMap<Character, Character> h = new HashMap<>();
+
+        h.put('(', ')');
+        h.put('[', ']');
+        h.put('{', '}');
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+            {
+                open.push(s.charAt(i));
+            }
+            else if(open.isEmpty())
+            {
+                return false;
+            }
+            else if(s.charAt(i) == h.get(open.peek()))
+            {
+                open.pop();
+            }
+            else{
+                return false;
+            }
+        }
+
+        return open.isEmpty();
+    }
+}
